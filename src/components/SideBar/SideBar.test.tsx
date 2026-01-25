61c98e30fa78ef05e9ed1d14cfdc9f35af543ff6
CharlieM312 <charliecmeader@gmail.com>
2026-01-25 22:03:57 +0000

    New builder using vite and tests changed to vitest for increased speed

--- src/components/SideBar/SideBar.test.tsx
+++ src/components/SideBar/SideBar.test.tsx
@@ -1,31 +1,32 @@
 import { fireEvent, render } from '@testing-library/react';
 import SideBar from './SideBar';
+import {  describe, test, expect, vi } from 'vitest';
 
 describe('Sidebar component', () => {
   test('Renders Hawk logo', () => {
     const { container } = render(<SideBar />);
 
     const logo = container.querySelector('img');
-    expect(logo).toHaveClass('logo');
+    expect(logo).toHaveAttribute('alt', 'logo');
   });
 
   test('Renders theme toggle svg', () => {
     const { container } = render(<SideBar />);
 
-    const toggle = container.querySelector('svg');
-    expect(toggle).toHaveClass('toggle');
+    const toggle = container.querySelector('svg#darkMode');
+    expect(toggle).toBeInTheDocument();
   });
 
   test('Changes theme when theme toggle is clicked', () => {
-    const spyLoStoGet = jest.spyOn(localStorage, 'getItem');
-    const spyLoStoSet = jest.spyOn(localStorage, 'setItem');
+    const spyLoStoGet = vi.spyOn(localStorage, 'getItem');
+    const spyLoStoSet = vi.spyOn(localStorage, 'setItem');
 
     const { container } = render(<SideBar />);
 
     expect(spyLoStoGet).toHaveBeenCalled();
     expect(localStorage.getItem('theme')).toBe('light');
 
-    const toggle = container.querySelector('svg');
+    const toggle = container.querySelector('svg#darkMode');
     toggle && fireEvent.click(toggle);
 
     expect(spyLoStoSet).toHaveBeenCalled();
