901326624a6db7930112b1438aa9539193280dbd
CharlieM312 <73425406+CharlieM312@users.noreply.github.com>
2026-02-11 13:57:22 +0000

    package updates and navigation updates to package and new table changes to stop navigation on instance not running (#24)

--- src/components/Table/Table.test.tsx
+++ src/components/Table/Table.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
+import * as RR from 'react-router';
 import Table from './Table';
 import Create from '../../js/client/Create';
 import Get from '../../js/instances/Get';
@@ -40,4 +41,39 @@ describe('Table component', () => {
     const table = container.querySelector('table');
     expect(table).toBeInTheDocument();
   });
+
+  test('Renders table with mock instances', () => {
+    const mockGet = vi.mocked(Get);
+    const mockCreate = vi.mocked(Create);
+
+    mockCreate.mockImplementation(() => ({} as any));
+    // @ts-ignore
+    mockGet.mockImplementation(() => ([{name: 'instance name', state: 'state', message: 'message'}]));
+
+    const { container } = render(<><MemoryRouter><Table url='http://avalidurl:3000' /></MemoryRouter></>);
+    const instanceName = container.querySelector('td');
+    expect(instanceName).toHaveTextContent('instance name');
+  });
+
+  test('Navigates to instance details page on row click', () => {
+    const mockGet = vi.mocked(Get);
+    const mockCreate = vi.mocked(Create);
+    const mockNavigate = vi.fn();
+
+    mockCreate.mockImplementation(() => ({} as any));
+    // @ts-ignore
+    mockGet.mockImplementation(() => ([{name: 'instancename', state: '0', message: 'message'}]));
+    vi.spyOn(RR, 'useNavigate').mockReturnValue(mockNavigate);
+    vi.spyOn(RR, 'useLocation').mockReturnValue({ pathname: '/instance' } as any);
+    const { container } = render(<><MemoryRouter><Table url='http://avalidurl:3000' /></MemoryRouter></>);
+
+    const row = container.querySelector('tbody tr');
+    if (row) {
+      (row as HTMLElement).click();
+    }
+
+    expect(mockNavigate).toHaveBeenCalledWith("/instance/instancename", { state: { instance: { name: 'instancename', status: 'RUNNING', info: 'message' }, url: 'http://avalidurl:3000' } });
+  });
+
 });
+
